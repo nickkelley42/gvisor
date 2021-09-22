8f75cb6668f36e2b52d2813e702383ff00e04e69
Bhasker Hariharan <bhaskerh@google.com>
2021-09-22 12:05:17 -0700

    Add server implementation for sharedmem endpoints.
    PiperOrigin-RevId: 398297110

--- pkg/tcpip/link/sharedmem/sharedmem_server.go
+++ pkg/tcpip/link/sharedmem/sharedmem_server.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The gVisor Authors.
+// Copyright 2021 The gVisor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,83 +15,21 @@
 //go:build linux
 // +build linux
 
-// Package sharedmem provides the implemention of data-link layer endpoints
-// backed by shared memory.
-//
-// Shared memory endpoints can be used in the networking stack by calling New()
-// to create a new endpoint, and then passing it as an argument to
-// Stack.CreateNIC().
 package sharedmem
 
 import (
-	"fmt"
 	"sync/atomic"
 
 	"golang.org/x/sys/unix"
-	"gvisor.dev/gvisor/pkg/log"
 	"gvisor.dev/gvisor/pkg/sync"
 	"gvisor.dev/gvisor/pkg/tcpip"
 	"gvisor.dev/gvisor/pkg/tcpip/buffer"
 	"gvisor.dev/gvisor/pkg/tcpip/header"
 	"gvisor.dev/gvisor/pkg/tcpip/link/rawfile"
-	"gvisor.dev/gvisor/pkg/tcpip/link/sharedmem/queue"
 	"gvisor.dev/gvisor/pkg/tcpip/stack"
 )
 
-// QueueConfig holds all the file descriptors needed to describe a tx or rx
-// queue over shared memory. It is used when creating new shared memory
-// endpoints to describe tx and rx queues.
-type QueueConfig struct {
-	// DataFD is a file descriptor for the file that contains the data to
-	// be transmitted via this queue. Descriptors contain offsets within
-	// this file.
-	DataFD int
-
-	// EventFD is a file descriptor for the event that is signaled when
-	// data is becomes available in this queue.
-	EventFD int
-
-	// TxPipeFD is a file descriptor for the tx pipe associated with the
-	// queue.
-	TxPipeFD int
-
-	// RxPipeFD is a file descriptor for the rx pipe associated with the
-	// queue.
-	RxPipeFD int
-
-	// SharedDataFD is a file descriptor for the file that contains shared
-	// state between the two ends of the queue. This data specifies, for
-	// example, whether EventFD signaling is enabled or disabled.
-	SharedDataFD int
-}
-
-// FDs returns the FD's in the QueueConfig as a slice of ints. This must
-// be used in conjunction with QueueConfigFromFDs to ensure the order
-// of FDs matches when reconstructing the config when serialized or sent
-// as part of control messages.
-func (q *QueueConfig) FDs() []int {
-	return []int{q.DataFD, q.EventFD, q.TxPipeFD, q.RxPipeFD, q.SharedDataFD}
-}
-
-// QueueConfigFromFDs constructs a QueueConfig out of a slice of ints where each
-// entry represents an file descriptor. The order of FDs in the slice must be in
-// the order specified below for the config to be valid. QueueConfig.FDs()
-// should be used when the config needs to be serialized or sent as part of a
-// control message to ensure the correct order.
-func QueueConfigFromFDs(fds []int) (QueueConfig, error) {
-	if len(fds) < 5 {
-		return QueueConfig{}, fmt.Errorf("insufficient number of fds: len(fds): %d, want: 5", len(fds))
-	}
-	return QueueConfig{
-		DataFD:       fds[0],
-		EventFD:      fds[1],
-		TxPipeFD:     fds[2],
-		RxPipeFD:     fds[3],
-		SharedDataFD: fds[4],
-	}, nil
-}
-
-type endpoint struct {
+type serverEndpoint struct {
 	// mtu (maximum transmission unit) is the maximum size of a packet.
 	mtu uint32
 
@@ -102,7 +40,7 @@ type endpoint struct {
 	addr tcpip.LinkAddress
 
 	// rx is the receive queue.
-	rx rx
+	rx serverRx
 
 	// stopRequested is to be accessed atomically only, and determines if
 	// the worker goroutines should stop.
@@ -115,7 +53,7 @@ type endpoint struct {
 	mu sync.Mutex
 
 	// tx is the transmit queue.
-	tx tx
+	tx serverTx
 
 	// workerStarted specifies whether the worker goroutine was started.
 	workerStarted bool
@@ -125,21 +63,21 @@ type endpoint struct {
 	peerFD int
 }
 
-// New creates a new shared-memory-based endpoint. Buffers will be broken up
+// NewServerEndpoint creates a new shared-memory-based endpoint. Buffers will be broken up
 // into buffers of "bufferSize" bytes.
-func New(mtu, bufferSize uint32, addr tcpip.LinkAddress, tx, rx QueueConfig, peerFD int, ClosedFunc func()) (stack.LinkEndpoint, error) {
-	e := &endpoint{
+func NewServerEndpoint(mtu, bufferSize uint32, addr tcpip.LinkAddress, tx, rx QueueConfig, peerFD int, ClosedFunc func()) (stack.LinkEndpoint, error) {
+	e := &serverEndpoint{
 		mtu:        mtu,
 		bufferSize: bufferSize,
 		addr:       addr,
 		peerFD:     peerFD,
 	}
 
-	if err := e.tx.init(bufferSize, &tx); err != nil {
+	if err := e.tx.init(&rx); err != nil {
 		return nil, err
 	}
 
-	if err := e.rx.init(bufferSize, &rx); err != nil {
+	if err := e.rx.init(&tx); err != nil {
 		e.tx.cleanup()
 		return nil, err
 	}
@@ -158,7 +96,7 @@ func New(mtu, bufferSize uint32, addr tcpip.LinkAddress, tx, rx QueueConfig, pee
 }
 
 // Close frees all resources associated with the endpoint.
-func (e *endpoint) Close() {
+func (e *serverEndpoint) Close() {
 	// Tell dispatch goroutine to stop, then write to the eventfd so that
 	// it wakes up in case it's sleeping.
 	atomic.StoreUint32(&e.stopRequested, 1)
@@ -178,13 +116,13 @@ func (e *endpoint) Close() {
 
 // Wait implements stack.LinkEndpoint.Wait. It waits until all workers have
 // stopped after a Close() call.
-func (e *endpoint) Wait() {
+func (e *serverEndpoint) Wait() {
 	e.completed.Wait()
 }
 
 // Attach implements stack.LinkEndpoint.Attach. It launches the goroutine that
 // reads packets from the rx queue.
-func (e *endpoint) Attach(dispatcher stack.NetworkDispatcher) {
+func (e *serverEndpoint) Attach(dispatcher stack.NetworkDispatcher) {
 	e.mu.Lock()
 	if !e.workerStarted && atomic.LoadUint32(&e.stopRequested) == 0 {
 		e.workerStarted = true
@@ -198,7 +136,7 @@ func (e *endpoint) Attach(dispatcher stack.NetworkDispatcher) {
 }
 
 // IsAttached implements stack.LinkEndpoint.IsAttached.
-func (e *endpoint) IsAttached() bool {
+func (e *serverEndpoint) IsAttached() bool {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	return e.workerStarted
@@ -206,29 +144,29 @@ func (e *endpoint) IsAttached() bool {
 
 // MTU implements stack.LinkEndpoint.MTU. It returns the value initialized
 // during construction.
-func (e *endpoint) MTU() uint32 {
+func (e *serverEndpoint) MTU() uint32 {
 	return e.mtu - header.EthernetMinimumSize
 }
 
 // Capabilities implements stack.LinkEndpoint.Capabilities.
-func (*endpoint) Capabilities() stack.LinkEndpointCapabilities {
+func (*serverEndpoint) Capabilities() stack.LinkEndpointCapabilities {
 	return stack.CapabilityRXChecksumOffload | stack.CapabilityTXChecksumOffload
 }
 
-// MaxHeaderLength implements stack.LinkEndpoint.MaxHeaderLength. It returns the
+// MaxHeaderLength implements stack.LinkEndpoint.MaxHeaderLength. It returns th0e
 // ethernet frame header size.
-func (*endpoint) MaxHeaderLength() uint16 {
+func (*serverEndpoint) MaxHeaderLength() uint16 {
 	return header.EthernetMinimumSize
 }
 
 // LinkAddress implements stack.LinkEndpoint.LinkAddress. It returns the local
 // link address.
-func (e *endpoint) LinkAddress() tcpip.LinkAddress {
+func (e *serverEndpoint) LinkAddress() tcpip.LinkAddress {
 	return e.addr
 }
 
 // AddHeader implements stack.LinkEndpoint.AddHeader.
-func (e *endpoint) AddHeader(local, remote tcpip.LinkAddress, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) {
+func (e *serverEndpoint) AddHeader(local, remote tcpip.LinkAddress, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) {
 	// Add ethernet header if needed.
 	eth := header.Ethernet(pkt.LinkHeader().Push(header.EthernetMinimumSize))
 	ethHdr := &header.EthernetFields{
@@ -246,14 +184,15 @@ func (e *endpoint) AddHeader(local, remote tcpip.LinkAddress, protocol tcpip.Net
 }
 
 // WriteRawPacket implements stack.LinkEndpoint.
-func (*endpoint) WriteRawPacket(*stack.PacketBuffer) tcpip.Error { return &tcpip.ErrNotSupported{} }
+func (*serverEndpoint) WriteRawPacket(*stack.PacketBuffer) tcpip.Error {
+	return &tcpip.ErrNotSupported{}
+}
 
-func (e *endpoint) writePacketLocked(r stack.RouteInfo, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) tcpip.Error {
+func (e *serverEndpoint) writePacketLocked(r stack.RouteInfo, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) tcpip.Error {
 	e.AddHeader(r.LocalLinkAddress, r.RemoteLinkAddress, protocol, pkt)
 
 	views := pkt.Views()
-	// Transmit the packet.
-	ok := e.tx.transmit(views...)
+	ok := e.tx.transmit(views)
 	if !ok {
 		return &tcpip.ErrWouldBlock{}
 	}
@@ -263,7 +202,8 @@ func (e *endpoint) writePacketLocked(r stack.RouteInfo, protocol tcpip.NetworkPr
 
 // WritePacket writes outbound packets to the file descriptor. If it is not
 // currently writable, the packet is dropped.
-func (e *endpoint) WritePacket(r stack.RouteInfo, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) tcpip.Error {
+func (e *serverEndpoint) WritePacket(r stack.RouteInfo, protocol tcpip.NetworkProtocolNumber, pkt *stack.PacketBuffer) tcpip.Error {
+	// Transmit the packet.
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	if err := e.writePacketLocked(r, protocol, pkt); err != nil {
@@ -274,7 +214,7 @@ func (e *endpoint) WritePacket(r stack.RouteInfo, protocol tcpip.NetworkProtocol
 }
 
 // WritePackets implements stack.LinkEndpoint.WritePackets.
-func (e *endpoint) WritePackets(r stack.RouteInfo, pkts stack.PacketBufferList, protocol tcpip.NetworkProtocolNumber) (int, tcpip.Error) {
+func (e *serverEndpoint) WritePackets(r stack.RouteInfo, pkts stack.PacketBufferList, protocol tcpip.NetworkProtocolNumber) (int, tcpip.Error) {
 	n := 0
 	var err tcpip.Error
 	e.mu.Lock()
@@ -296,40 +236,13 @@ func (e *endpoint) WritePackets(r stack.RouteInfo, pkts stack.PacketBufferList, 
 
 // dispatchLoop reads packets from the rx queue in a loop and dispatches them
 // to the network stack.
-func (e *endpoint) dispatchLoop(d stack.NetworkDispatcher) {
-	// Post initial set of buffers.
-	limit := e.rx.q.PostedBuffersLimit()
-	if l := uint64(len(e.rx.data)) / uint64(e.bufferSize); limit > l {
-		limit = l
-	}
-	for i := uint64(0); i < limit; i++ {
-		b := queue.RxBuffer{
-			Offset: i * uint64(e.bufferSize),
-			Size:   e.bufferSize,
-			ID:     i,
-		}
-		if !e.rx.q.PostBuffers([]queue.RxBuffer{b}) {
-			log.Warningf("Unable to post %v-th buffer", i)
-		}
-	}
-
-	// Read in a loop until a stop is requested.
-	var rxb []queue.RxBuffer
+func (e *serverEndpoint) dispatchLoop(d stack.NetworkDispatcher) {
 	for atomic.LoadUint32(&e.stopRequested) == 0 {
-		var n uint32
-		rxb, n = e.rx.postAndReceive(rxb, &e.stopRequested)
-
-		// Copy data from the shared area to its own buffer, then
-		// prepare to repost the buffer.
-		b := make([]byte, n)
-		offset := uint32(0)
-		for i := range rxb {
-			copy(b[offset:], e.rx.data[rxb[i].Offset:][:rxb[i].Size])
-			offset += rxb[i].Size
-
-			rxb[i].Size = e.bufferSize
+		b := e.rx.receive()
+		if b == nil {
+			e.rx.waitForPackets()
+			continue
 		}
-
 		pkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
 			Data: buffer.View(b).ToVectorisedView(),
 		})
@@ -339,7 +252,6 @@ func (e *endpoint) dispatchLoop(d stack.NetworkDispatcher) {
 			continue
 		}
 		eth := header.Ethernet(hdr)
-
 		// Send packet up the stack.
 		d.DeliverNetworkPacket(eth.SourceAddress(), eth.DestinationAddress(), eth.Type(), pkt)
 	}
@@ -352,6 +264,6 @@ func (e *endpoint) dispatchLoop(d stack.NetworkDispatcher) {
 }
 
 // ARPHardwareType implements stack.LinkEndpoint.ARPHardwareType
-func (*endpoint) ARPHardwareType() header.ARPHardwareType {
+func (*serverEndpoint) ARPHardwareType() header.ARPHardwareType {
 	return header.ARPHardwareEther
 }
